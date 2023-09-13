28ff0a81fe108c9e17caed9171b9eb70fbfb0cda
gustavo.rocha <gustavorocha.net@hotmail.com>
2023-09-13 00:24:04 -0300

    implantação do desafio

--- src/controleDeFluxo/Contador.java
+++ src/controleDeFluxo/Contador.java
@@ -1,7 +1,41 @@
 package controleDeFluxo;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+class ParametrosInvalidosException extends Exception {
+}
+
 public class Contador {
 	public static void main(String[] args) {
-		System.out.println("Olá mundo");
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+		System.out.println("Digite o primeiro número: ");
+		int parametroUm = scanner.nextInt();
+
+		System.out.println("Digite o segundo número: ");
+		int parametroDois = scanner.nextInt();
+
+		try {
+			contar(parametroUm, parametroDois);
+		} catch (ParametrosInvalidosException e) {
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		}
+
+	}
+
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		{
+
+			if (parametroUm > parametroDois) {
+				throw new ParametrosInvalidosException();
+			}
+			
+			int contagem = parametroDois - parametroUm;
+			for (int contando = 1; contando <= contagem; contando++) {
+				System.out.println("Imprimindo o número " + contando);
+			}
+
+		}
 	}
 }
